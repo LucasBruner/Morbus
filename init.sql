e0665967dc04b79aa3ef0b4a392bad5fadda5683
Lucas Bruner <lucas-bruner@hotmai.com.br>
2026-06-02 00:11:04 -0300

    add docker file orchestrator

---
+++ init.sql
@@ -0,0 +1,11 @@
+-- Executado automaticamente como superuser (postgres) na criação do container.
+-- Cria o usuário da aplicação e concede todos os privilégios necessários
+-- para que o Flyway e o Hibernate possam criar e gerenciar as tabelas.
+
+CREATE USER sus_user WITH PASSWORD 'sus_pass';
+
+GRANT ALL PRIVILEGES ON DATABASE sus_queue_db TO sus_user;
+GRANT ALL ON SCHEMA public TO sus_user;
+ALTER SCHEMA public OWNER TO sus_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO sus_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO sus_user;
