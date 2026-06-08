c089973f92e77d2c3134424445d990bf0f95cb21
LucasBruner <45501266+LucasBruner@users.noreply.github.com>
2026-06-08 19:40:13 -0300

    Bruner (#58)
    * add migration flyway, resolve #20
    
    * create mvc structure, resolve #28
    
    * adjust some issues
    
    * implements notification, resolve #29
    
    * use smallrye invés de camel spring e criar as bindings, resolve #30
    
    ---------
    
    Co-authored-by: Lucas Bruner <lucas-bruner@hotmai.com.br>

--- notification-service/src/main/java/br/com/sus/notificationservice/consumer/QueueEventConsumer.java
+++ notification-service/src/main/java/br/com/sus/notificationservice/consumer/QueueEventConsumer.java
@@ -1,4 +1,29 @@
 package br.com.sus.notificationservice.consumer;
 
+import br.com.sus.notificationservice.model.dto.QueueEventDTO;
+import br.com.sus.notificationservice.service.NotificationService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
 public class QueueEventConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(QueueEventConsumer.class);
+
+    @Inject
+    NotificationService notificationService;
+
+    @Incoming("queue-events")
+    public void consume(QueueEventDTO event) {
+        try {
+            log.info("[CONSUMER] Evento recebido: {} para {}", event, event.patientName());
+            notificationService.process(event);
+            log.info("[CONSUMER] Evento processado com sucesso");
+        } catch (Exception e) {
+            log.error("[CONSUMER] Erro ao processar evento", e);
+        }
+    }
 }
