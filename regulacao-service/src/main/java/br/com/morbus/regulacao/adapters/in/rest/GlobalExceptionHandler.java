c07c00fb9f2acec4e32b3ec57ed2ae2795fdea84
Lucas Bruner <lucas-bruner@hotmail.com.br>
2026-06-22 20:33:39 -0300

    configurar filtro jwt, resolve #69

---
+++ regulacao-service/src/main/java/br/com/morbus/regulacao/adapters/in/rest/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package br.com.morbus.regulacao.adapters.in.rest;
+
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleValidation(MethodArgumentNotValidException ex) {
+        String detail = ex.getBindingResult().getFieldErrors().stream()
+                .map(e -> e.getField() + ": " + e.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        ProblemDetail problem = ProblemDetail.forStatus(422);
+        problem.setType(URI.create("https://httpstatuses.com/422"));
+        problem.setTitle("Dados inválidos");
+        problem.setDetail(detail);
+        return problem;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleGeneric(Exception ex) {
+        ProblemDetail problem = ProblemDetail.forStatus(500);
+        problem.setType(URI.create("https://httpstatuses.com/500"));
+        problem.setTitle("Erro interno");
+        problem.setDetail(ex.getMessage());
+        return problem;
+    }
+}
