f1f247b255cacc8e3a373cc194995b5e57d67aff
Brenda Bernat <brendalouisebernat@gmail.com>
2026-07-01 23:52:20 -0300

    Fix issues

--- agendamento-service/src/main/java/br/com/morbus/agendamento/domain/exception/InvalidSlotStatusException.java
+++
@@ -1,8 +0,0 @@
-package br.com.morbus.agendamento.domain.exception;
-
-public class InvalidSlotStatusException extends RuntimeException {
-
-    public InvalidSlotStatusException(String message) {
-        super(message);
-    }
-}
